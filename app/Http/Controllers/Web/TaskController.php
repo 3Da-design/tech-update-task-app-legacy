27d62d5a104a1a4b9c197a50593f16d1a3d440c9
Y.S <184586909+Yuto-design@users.noreply.github.com>
2026-06-27 14:16:20 +0900

    ordered_imports

--- app/Http/Controllers/Web/TaskController.php
+++ app/Http/Controllers/Web/TaskController.php
@@ -7,11 +7,11 @@ use App\Http\Requests\IndexTaskRequest;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class TaskController extends Controller
