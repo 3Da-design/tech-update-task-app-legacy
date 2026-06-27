819309ed3a243b1d7196763a75d7c917ad8df6f5
Y.S <184586909+Yuto-design@users.noreply.github.com>
2026-06-27 13:40:58 +0900

    change Tab Size 4 -> 2

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,20 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/tasks');
 
 Route::get('/dashboard', fn () => redirect()->route('tasks.index'))
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+  ->middleware(['auth', 'verified'])
+  ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-    Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit')->whereNumber('id');
-    Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update')->whereNumber('id');
-    Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy')->whereNumber('id');
+  Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+  Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+  Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+  Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit')->whereNumber('id');
+  Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update')->whereNumber('id');
+  Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy')->whereNumber('id');
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
